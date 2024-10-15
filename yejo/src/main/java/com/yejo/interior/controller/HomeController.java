d6bdf698309273f2b836fcee9f2bce31a494dfc0
HHS <77200506+HHS-1@users.noreply.github.com>
2024-10-15 12:39:59 +0900

    이거 가져가세요

--- yejo/src/main/java/com/yejo/interior/controller/HomeController.java
+++ yejo/src/main/java/com/yejo/interior/controller/HomeController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.yejo.interior.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,4 +10,5 @@ public class HomeController {
 	public String adminIndexPage() {
 		return "admin/index";
 	}
+	
 }
