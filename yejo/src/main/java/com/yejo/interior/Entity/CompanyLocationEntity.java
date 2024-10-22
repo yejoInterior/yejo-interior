45cebbfd6a087b38b0eee348a465a73da8046aec
airmungs <airmungs@gmail.com>
2024-10-22 21:12:17 +0900

    일단 제ㅐ작중

---
+++ yejo/src/main/java/com/yejo/interior/Entity/CompanyLocationEntity.java
@@ -0,0 +1,26 @@
+package com.yejo.interior.Entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class CompanyLocationEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+    private String tel;
+    private String safeTel;
+    private String email;
+    private String naverMapUrl;
+    private String kakaoMapUrl;
+
+}
