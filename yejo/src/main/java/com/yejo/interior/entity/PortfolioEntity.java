1ef5152d6106ef4a0bcb2fbd7417b709cb9d4b41
airmungs <airmungs@gmail.com>
2024-10-25 14:37:20 +0900

    Fix cache issue

---
+++ yejo/src/main/java/com/yejo/interior/entity/PortfolioEntity.java
@@ -0,0 +1,41 @@
+package com.yejo.interior.entity;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class PortfolioEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String location;
+    private Double area; // 면적
+    private String duration; // 시공기간
+    private String type; // 타입
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<PortfolioImageEntity> images;
+
+    @Column(name = "thumbnail_image")
+    private String thumbnailImage; // 썸네일 경로
+
+}
+
+
+
---
+++ yejo/src/main/java/com/yejo/interior/repository/PortfolioRepository.java
@@ -0,0 +1,10 @@
+package com.yejo.interior.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yejo.interior.entity.PortfolioEntity;
+
+public interface PortfolioRepository extends JpaRepository<PortfolioEntity, Long> {
+}
+
+
