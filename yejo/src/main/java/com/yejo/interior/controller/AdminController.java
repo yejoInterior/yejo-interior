5817ebc1afa613ca1ae111414bc5ac0c992ffbe7
Donggeon0304 <rodzld18@naver.com>
2024-11-26 22:15:35 +0900

    session 설정

--- yejo/src/main/java/com/yejo/interior/controller/AdminController.java
+++ yejo/src/main/java/com/yejo/interior/controller/AdminController.java
@@ -1,16 +1,15 @@
 package com.yejo.interior.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.*;
 import com.yejo.interior.entity.BannerEntity;
 import com.yejo.interior.entity.CompanyLocationEntity;
 import com.yejo.interior.entity.ConsultantEntity;
@@ -26,6 +25,8 @@ import com.yejo.interior.service.PopupService;
 import com.yejo.interior.service.PortfolioService;
 import com.yejo.interior.service.ReviewService;
 import com.yejo.interior.service.YejoStoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
@@ -50,44 +51,74 @@ public class AdminController {
 	private KakaoTalkService kakaoTalkService;
 	
 	@GetMapping("/")
-	public String main() {
+	public String main(HttpSession session) {
+		session.invalidate();
 		return "admin/main";
 	}
 	
 	@GetMapping("/banner")
-	public String bannerPage(Model model) {
-		List<BannerEntity> bannerList = bannerService.getBanner();
-		model.addAttribute("bannerList",bannerList);
-		return "admin/banner";
+	public String bannerPage(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	List<BannerEntity> bannerList = bannerService.getBanner();
+    		model.addAttribute("bannerList",bannerList);
+    		return "admin/banner";
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
+		
 	}
 	
 	@GetMapping("/estimate")
-	public String estimatePage(Model model) {
-		List<ConsultantEntity> estimateList = consultantService.getAllEstimate();
-		model.addAttribute("estimateList",estimateList);
-		return "admin/estimate";
+	public String estimatePage(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	List<ConsultantEntity> estimateList = consultantService.getAllEstimate();
+    		model.addAttribute("estimateList",estimateList);
+    		return "admin/estimate";
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
+		
 	}
 	
 	@GetMapping("/about")
-	public String about(Model model) {
-		YejoStoryEntity yejoStory=yejoStoryService.getIntroduction();
-        model.addAttribute("savedIntroductionText", yejoStory.getIntroductionText());
-        model.addAttribute("savedImagePath", yejoStory.getImagePath());
-		return "admin/about";
+	public String about(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	YejoStoryEntity yejoStory=yejoStoryService.getIntroduction();
+            model.addAttribute("savedIntroductionText", yejoStory.getIntroductionText());
+            model.addAttribute("savedImagePath", yejoStory.getImagePath());
+    		return "admin/about";
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
+		
 	}
 	
     @GetMapping("/portfolio")
-    public String getPortfolioList(Model model) {
-        List<PortfolioEntity> portfolioList = portfolioService.getAllPortfolios();
-        model.addAttribute("portfolioList", portfolioList);
-        return "admin/portfolio"; // 뷰 이름
+    public String getPortfolioList(Model model, HttpSession session) {
+    	Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	List<PortfolioEntity> portfolioList = portfolioService.getAllPortfolios();
+            model.addAttribute("portfolioList", portfolioList);
+            return "admin/portfolio"; // 뷰 이름
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
+        
     }
 	
 	@GetMapping("/location")
-	public String showLocationPage(Model model) {
-	    CompanyLocationEntity location = locationService.getLocation();
-	    model.addAttribute("location", location);
-	    return "admin/location"; // 템플릿 경로
+	public String showLocationPage(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	CompanyLocationEntity location = locationService.getLocation();
+    	    model.addAttribute("location", location);
+    	    return "admin/location"; // 템플릿 경로
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
 	}
 
 	
@@ -117,18 +148,29 @@ public class AdminController {
 	}
 	
 	@GetMapping("/review-management")
-	public String reviewManagementPage(Model model) {
-		List<Review> reviews = reviewService.getAllReview();
-		model.addAttribute("reviews",reviews);
-		return "admin/review-management";
+	public String reviewManagementPage(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	List<Review> reviews = reviewService.getAllReview();
+    		model.addAttribute("reviews",reviews);
+    		return "admin/review-management";
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
+		
 	}
 	
 	@GetMapping("/popup")
-	public String popupPage(Model model) {
-		Optional<PopupEntity> popupOptional = popupService.getPopup();
-		PopupEntity popup = popupOptional.orElse(null); // Optional 해제
-	    model.addAttribute("popup", popup);
-		return "admin/popup";
+	public String popupPage(Model model, HttpSession session) {
+		Boolean checkIn = (Boolean) session.getAttribute("checkIn");
+        if (checkIn != null && checkIn) {
+        	Optional<PopupEntity> popupOptional = popupService.getPopup();
+    		PopupEntity popup = popupOptional.orElse(null); // Optional 해제
+    	    model.addAttribute("popup", popup);
+    		return "admin/popup";
+        } else {
+            return "redirect:/admin/";  // 로그인 안 된 경우 로그인 페이지로 리다이렉트
+        }
 	}
 	
 	@Value("${kakao.restApiKey}")
@@ -150,6 +192,20 @@ public class AdminController {
 	    kakaoTalkService.createToken(authorizationCode);
 	    return "admin/banner";  
 	}
+	
+    // 로그인 처리 POST 요청
+	@PostMapping("/login")
+	@ResponseBody
+	public ResponseEntity<?> login(@RequestBody Map<String, String> requestData, HttpSession session) {
+	    String password = requestData.get("password");  // 클라이언트에서 전송한 비밀번호 받기
+	    if ("yejo7048@".equals(password)) {  // 비밀번호가 맞으면
+	        session.setAttribute("checkIn", true);  // 세션에 로그인 정보 저장
+	        return ResponseEntity.ok().body("{\"success\": true}");  // 로그인 성공 응답
+	    } else {
+	        return ResponseEntity.ok().body("{\"success\": false}");  // 로그인 실패 응답
+	    }
+	}
+
 
 	
 }
